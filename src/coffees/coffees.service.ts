bfb235ce683cfb2806d9da048da1b52ddc787510
Nathan Dolzonek <natedolz@gmail.com>
2022-02-07 14:46:58 -0600

    finish chapter 2

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Pike Place Roast',
+      brand: 'Starbucks',
+      flavors: ['light', 'nutty'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee ${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: CreateCoffeeDto) {
+    this.coffees.push({ ...createCoffeeDto, id: this.coffees.length + 1 });
+    return createCoffeeDto;
+  }
+
+  update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      existingCoffee.name = updateCoffeeDto.name ?? existingCoffee.name;
+      existingCoffee.brand = updateCoffeeDto.brand ?? existingCoffee.brand;
+      existingCoffee.flavors =
+        updateCoffeeDto.flavors ?? existingCoffee.flavors;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
